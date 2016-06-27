ae0c4322927bf07d90ad1682e6cb4278425f1a46
Jamie <Admin@STUDENT15>
2016-06-27 14:49:01 +0100

    Changes to booking

--- QAcinemas/QACinemasWebsite/Booking/SelectSeats.aspx.cs
+++ QAcinemas/QACinemasWebsite/Booking/SelectSeats.aspx.cs
@@ -13,18 +13,33 @@ namespace QACinemasWebsite.Booking
         {
             //Then use the cinema id and film id retrieved to display that info too
             Session["ShowingID"] = Request["sID"];
+            //Session["SeatPrice"] = string.Format("{0:C}", Convert.ToDecimal(Request["price"]));
+            Session["SeatPrice"] = Request["price"];
 
             DataSetTableAdapters.ShowingsTableAdapter showingsAdapter = new DataSetTableAdapters.ShowingsTableAdapter();
             //DataSet.ShowingsDataTable showingsData = showingsAdapter.GetShowingsByCinemaId(Session["ShowingID"].ToString());
             //Test*********************************************
-            DataSet.ShowingsDataTable showingsData = showingsAdapter.GetData();
+            DataSet.ShowingsDataTable showingsData = showingsAdapter.GetShowingByShowingId(Convert.ToInt64(Session["ShowingID"].ToString()));
+
+            DataSetTableAdapters.ScreensTableAdapter screenAdapter = new DataSetTableAdapters.ScreensTableAdapter();
+            DataSet.ScreensDataTable screenData = screenAdapter.GetScreenByScreenId(showingsData[0].ScreenId, true);
 
             DataSetTableAdapters.FilmsTableAdapter taFilms = new DataSetTableAdapters.FilmsTableAdapter();
             DataSet.FilmsDataTable fData = taFilms.GetFilmById(Convert.ToInt64(Session["SelectedMovieID"]));
+            DataSet.ScreensRow row1 = showingsData[0].ScreensRow;
+          
+            if (screenData[0].Deluxe == false)
+            {
+                Session["ScreenType"] = "Standard";
+            }
+            else
+            {
+                Session["ScreenType"] = "Deluxe";
+            }
 
-            //Format the start times*************************
-            lblFilmStuff.Text = "Booking for the " + showingsData[0].StartTime + " showing of " + fData[0].Title + " on " + showingsData[0].StartTime;
-
+            Session["ShowingDateTime"] = showingsData[0].StartTime;
+            lblFilmStuff.Text = "Booking for the " + showingsData[0].StartTime.ToString("H:mm") + " showing of " + fData[0].Title + " in a " + Session["ScreenType"] + " screen on " + showingsData[0].StartTime.ToString("yyyy'-'MM'-'dd");
+            lblFilmStuff.Text += " " + Session["SeatPrice"];
         }
 
         protected void dlstNoSeats_SelectedIndexChanged(object sender, EventArgs e)
@@ -35,6 +50,13 @@ namespace QACinemasWebsite.Booking
         protected void btnToPay_Click(object sender, EventArgs e)
         {
             Session["NoSeats"] = dlstNoSeats.SelectedValue;
+            string[] seats = new string[Convert.ToInt64(dlstNoSeats.SelectedValue)];
+            for(int i = 0; i < Convert.ToInt64(dlstNoSeats.SelectedValue); i++)
+            {
+                seats[i] = "a" + i;
+            }
+            Session["Seats"] = seats;
+            //string[] seat2 = Session["Seats"] as string[];
             Response.Redirect("PaypalPlaceholder.aspx");
         }
     }
