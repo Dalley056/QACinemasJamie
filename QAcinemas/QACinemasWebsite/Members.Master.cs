9f17f24e8b109a5db44a54d2804c8b83e99773ed
Ian Owen <ioawnen@gmail.com>
2017-03-01 16:22:46 +0000

    Adds Members.Master

---
+++ QAcinemas/QACinemasWebsite/Members.Master.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QACinemasWebsite
+{
+    public partial class Members : System.Web.UI.MasterPage
+    {
+        protected void OnInit(object sender, EventArgs e)
+        {
+            //Triggered on page init, Used to check 
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
