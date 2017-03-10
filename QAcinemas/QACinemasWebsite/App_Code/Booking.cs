93a49cb08f9b2d9860aee3ccf24cf646660c6f19
Ian Owen <ioawnen@gmail.com>
2017-03-10 13:07:03 +0000

    General commenting and cleanup

--- QAcinemas/QACinemasWebsite/App_Code/Booking.cs
+++ QAcinemas/QACinemasWebsite/App_Code/Booking.cs
@@ -5,14 +5,20 @@ using System.Web;
 
 namespace QACinemasWebsite.App_Code
 {
+    /// <summary>
+    /// QACinemasWebsite booking operations class
+    /// </summary>
     public class Booking
     {
+        /// <summary>
+        /// Booking data storage object
+        /// </summary>
         public class BookingData
         {
             public int BookingId, UserId, ShowingId, BillingId;
             public int[] Seats;
 
-            public BookingData(int bookingId, int userId, int showingId, int billingId, int[] seats )
+            public BookingData(int bookingId, int userId, int showingId, int billingId, int[] seats)
             {
                 BookingId = bookingId;
                 UserId = userId;
